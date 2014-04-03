c6a78351b1866facd965cca4cdb30cb616162bf0
Chanikarn Prakunkorn <prakunchan@myvuw.ac.nz>
2014-04-04 10:58:40 +1300

    Error handling

--- quote.js
+++ quote.js
@@ -24,7 +24,7 @@ app.get('/quote/random', function(req, res) {
 app.get('/quote/:id', function(req, res) {
   if(quotes.length <= req.params.id || req.params.id < 0) {
     res.statusCode = 404;
-    var error.error = 'Error 404: No quote found';
+    var error = { error : 'Error 404: No quote found'};
     return res.send(error);
   }
 
